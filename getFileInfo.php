cbb80635a86f8f9d6593afe094d991924aabf18f
Danny <DanielCasanova@users.noreply.github.com>
2016-10-15 15:25:25 +0100

    Delete getFileInfo.php

--- getFileInfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $path = $_GET['path'];
-    $dir = './topLevel/'.$path;
-
-    $fInfo = array(pathinfo($dir)['extension'], filesize($dir), filemtime($dir));
-
-    echo json_encode($fInfo);
-?>
